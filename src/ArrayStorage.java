ed88bf1ac374f3772db6e2ecaa27fd12cb60176e
ZloyGremlinJE  email = rellepro@gmail.com <rellepro@gmail.com>
2018-06-29 15:05:10 +0300

    First solution

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,61 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+        for (int i = 0; i < size()-1; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+      Resume  result = null;
+        for (Resume resume : storage) {
+            if (resume == null) break;
+            if (resume.uuid.equals(uuid)) {
+                result = resume;
+            }
+        }
+
+        return result;
     }
 
     void delete(String uuid) {
+        int index = 0;
+        for (int i = 0; i < size()-1 ; i++) {
+            if(storage[i] == null) break;
+            if(storage[i].uuid == uuid){
+               index = i;
+            }
+        }
+        for (int i = index; i < size()-2 ; i++) {
+            storage[i] = storage[i+1];
+            if(storage[i] == null) break;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int index = size()-1;
+        for (int i = 0; i < size()-1 ; i++) {
+          if(storage[i] == null) {
+              index = i;
+              break;
+          }
+        }
+        Resume [] resumesWithoutNull = Arrays.copyOfRange(storage, 0, index);
+
+        return resumesWithoutNull;
     }
 
     int size() {
-        return 0;
+        return storage.length;
     }
 }
