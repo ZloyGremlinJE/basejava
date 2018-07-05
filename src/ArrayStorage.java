9a113a38f4ab24b4f3d63d1f0de5c2cb145e13db
ZloyGremlinJE  email = rellepro@gmail.com <rellepro@gmail.com>
2018-07-05 11:57:50 +0300

    убрал пересчет, исправлена правая граница в fill, убрал избыточную переменную в delete

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,13 +5,18 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
-        Arrays.fill(storage, 0, size() - 1, null);// обнуляем только ячейки в ктр. не null
+        //обнуляем только ячейки в ктр. не null, обнуляем кол-во резюме
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     void save(Resume r) {
-        storage[size()] = r;
+        storage[size] = r;
+        //инкрементируем кол-во резюме
+        size++;
     }
 
     Resume get(String uuid) {
@@ -24,13 +29,12 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        int size = size();
         for (int i = 0; i < size; i++) {
             /*Находим индекс удаляемого элемента и удаляем путем перезаписи на предыдущий элемент
-             всех не null элементов массива, идущих после удаляемого.*/
+             всех не null элементов массива, идущих после удаляемого. Декрементируем количество резюме*/
             if (storage[i].uuid.equals(uuid)) {
-                int removeIndex = i;
-                System.arraycopy(storage, removeIndex + 1, storage, removeIndex, size - removeIndex);
+                System.arraycopy(storage, i + 1, storage, i, size - i);
+                size--;
                 break;
             }
         }
@@ -40,16 +44,10 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage, size());
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        int size = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) {
-                size++;
-            } else break;
-        }
         return size;
     }
 }
