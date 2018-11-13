4d1202dd73c84f9cb28a36afbfb7ff443bb2f5db
ZloyGremlinJE <rellepro@gmail.com>
2018-11-13 15:45:28 +0300

    Lesson09 HW AbstractPathStorage realization v.2

--- src/ru/javawebinar/basejava/storage/AbstractPathStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractPathStorage.java
@@ -9,6 +9,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public abstract class AbstractPathStorage extends AbstractStorage<Path> {
     private Path directory;
@@ -23,13 +24,16 @@ public abstract class AbstractPathStorage extends AbstractStorage<Path> {
 
     @Override
     protected Path getSearchKey(String uuid) {
-
-        return null;
+        return directory.resolve(uuid);
     }
 
     @Override
     protected void doUpdate(Resume resume, Path path) {
-
+        try {
+            doWrite(resume, new BufferedOutputStream(new FileOutputStream(path.toFile())));
+        } catch (IOException e) {
+            throw new StorageException("Error writing file", path.toString(), e);
+        }
     }
 
     @Override
@@ -60,12 +64,22 @@ public abstract class AbstractPathStorage extends AbstractStorage<Path> {
 
     @Override
     protected void doDelete(Path path) {
-
+        try {
+            Files.delete(path);
+        } catch (IOException e) {
+            throw new StorageException("Error deleting file", path.toString());
+        }
     }
 
     @Override
     protected List<Resume> getCopyList() {
-       return null;
+        List<Resume> result = null;
+        try {
+            result = Files.list(directory).map(this::doGet).collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new StorageException("Path delete error", null);
+        }
+        return result;
     }
 
     @Override
